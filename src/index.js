d171bc72f2c4f62d82a108a76bb3f334ef949017
ivanmanan <ivanmanan@gmail.com>
2017-09-13 21:46:32 -0700

    Set-up the skeleton files.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const element = (
+  <h1>Hello world!</h1>
+);
+
+ReactDOM.render(element, document.getElementById('root'));
