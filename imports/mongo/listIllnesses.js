395d929e4167ec962f4c874641c585197c4cb791
ivanmanan <ivanmanan@gmail.com>
2017-09-17 15:36:29 -0700

    Added BMI image

--- imports/mongo/listIllnesses.js
+++ imports/mongo/listIllnesses.js
@@ -6,6 +6,6 @@
    mongo shell> load('/home/ivan/crux/imports/mongo/listIllnesses.js');
 */
 
-db.illnesses.insert({ name: "First illness", link: "#", createdAt: new Date() });
+db.illnesses.insert({ name: "Obesity", link: "en.wikipedia.org/wiki/Obesity", createdAt: new Date() });
 db.illnesses.insert({ name: "Second illness", link: "#", createdAt: new Date() });
 db.illnesses.insert({ name: "Third illness", link: "#", createdAt: new Date() });
