a5357857e46f737875d271c8be40844d6ba71507
ivanmanan <ivanmanan@gmail.com>
2017-09-22 00:23:28 -0700

    Updated search button

--- imports/Components/Symptoms.jsx
+++ imports/Components/Symptoms.jsx
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { createContainer } from 'meteor/react-meteor-data';
+import Encyclopedia from './Encyclopedia';
+
+import { Illnesses } from '../api/illness.js';
 
 class Symptoms extends Component {
+
+  handleClearSymptoms() {
+    return;
+  }
+
+
+  renderIllness() {
+    // Splits the input string into an array of words
+    var symptoms = this.props.symptoms.split(" ");
+
+    // Search for matching symptoms in database
+    // Then spit out the illness names
+
+    // Need to be able to query single words in the database for symptoms attribute
+    for (var i = 0; i != symptoms.length; i++) {
+      /* console.log(Illnesses.find({ symptoms: symptoms[i] }).fetch());*/
+    }
+
+    // need to do the return as an if-statement else there will be console errors
+    var illnessData = Illnesses.find({ symptoms: this.props.symptoms }).fetch();
+    return (
+      <Encyclopedia key={illnessData[0]._id} illness={illnessData[0]}/>
+    );
+  }
+
   render() {
     return (
       <div className="symptom">
+        <br/>
         <div className="row">
-          <div className="col-md-offset-1 col-sm-offset-1 col-xs-offset-1">
+          <div className="col-md-3 col-sm-5 col-xs-4
+            col-md-offset-1 col-sm-offset-1 col-xs-offset-1">
             <h1>Symptoms</h1>
+          </div>
+          <div className="col-md-3 col-sm-5 col-xs-5">
+            <button onClick={this.handleClearSymptoms}>
+              <p>Clear Symptoms</p>
+            </button>
           </div>
         </div>
         <hr/>
@@ -16,17 +52,23 @@ class Symptoms extends Component {
             <h2>{this.props.symptoms}</h2>
           </div>
         </div>
-      <hr/>
+        <hr/>
+
+        {this.renderIllness()}
       </div>
     );
   }
 }
 
-export default Symptoms;
-
+export default createContainer(() => {
+  return {
+    illnesses: Illnesses.find({}, { sort: { name: 1 } }).fetch(),
+  };
+}, Symptoms);
 /*
    TO-DO:
-   Then display cards for each symptom --> this will be another component
+   Need to perofrm queries on database WHERE you search for symptoms
 
+   Future:
    Enforce ability to 'X' out each symptom
  */
